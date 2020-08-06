c330f7d755392f056fc0c2501c9722e8b6daf71d
Ulu-home <aloha@ulumills.com>
2020-08-06 09:25:53 -0400

    Replaces tans with greys, adds time/greeting flair

--- wp-content/themes/ulu_port2/js/main.js
+++ wp-content/themes/ulu_port2/js/main.js
@@ -186,6 +186,113 @@ jQuery(document).ready(function ($) {
 
 
 
+// Time change text
+        var now = new Date();
+        var hours = now.getHours();
+
+
+        var msg;
+        var msgEng;
+        var msgJp;
+        if (hours < 12) {
+            msg = "Aloha kakahiaka!";
+            msgEng = "Good morning!";
+            msgJp = "おはよう!";
+        }
+        else if (hours < 18) {
+            msg = "Aloha 'auinala!";
+            msgEng = "Good afternoon!";
+        }
+
+
+        else msg = "Aloha ahiahi","Good evening";
+        // $('.greeting').text(msg);
+        // $('.greetingEng').text(msgEng);
+    // $('#greeting').attr("data-msg", msg);  //setter
+
+
+
+    // List of sentences
+    var _CONTENT = [ msgEng , msgJp , msg
+    ];
+
+// Current sentence being processed
+    var _PART = 0;
+
+// Character number of the current sentence being processed
+    var _PART_INDEX = 0;
+
+// Holds the handle returned from setInterval
+    var _INTERVAL_VAL;
+
+// Element that holds the text
+    var _ELEMENT = document.querySelector("#greeting");
+
+// Cursor element
+//     var _CURSOR = document.querySelector("#cursor");
+
+// Implements typing effect
+    function Type() {
+        // Get substring with 1 characater added
+        var text =  _CONTENT[_PART].substring(0, _PART_INDEX + 1);
+        _ELEMENT.innerHTML = text;
+        _PART_INDEX++;
+
+        // If full sentence has been displayed then start to delete the sentence after some time
+        if(_PART < (_CONTENT.length - 1)) {
+            if (text === _CONTENT[_PART]) {
+                // Hide the cursor
+                // _CURSOR.style.display = 'none';
+
+                clearInterval(_INTERVAL_VAL);
+                setTimeout(function () {
+                    _INTERVAL_VAL = setInterval(Delete, 75);
+                }, 2000);
+            }
+        }
+    }
+//ULU: This is me trying to get the deleting to stop on the last sentence.
+    if(_PART < (_CONTENT.length - 1)) {
+// Implements deleting effect
+    function Delete() {
+        // Get substring with 1 character deleted
+        var text =  _CONTENT[_PART].substring(0, _PART_INDEX - 1);
+        _ELEMENT.innerHTML = text;
+        _PART_INDEX--;
+
+        // If sentence has been deleted then start to display the next sentence
+        if(text === '') {
+            clearInterval(_INTERVAL_VAL);
+
+            // If current sentence was last then display the first one, else move to the next
+            // if(_PART == (_CONTENT.length - 1))
+            //     _PART = 0;
+            // else
+                _PART++;
+
+            _PART_INDEX = 0;
+
+            // Start to display the next sentence after some time
+            setTimeout(function() {
+                // _CURSOR.style.display = 'inline-block';
+                _INTERVAL_VAL = setInterval(Type, 100);
+            }, 200);
+        }
+    }
+    }
+
+// Start the typing effect on load
+    _INTERVAL_VAL = setInterval(Type, 100);
+
+
+
+
+
+
+
+
+
+
 
 
 
